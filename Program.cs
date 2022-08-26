f9d6cb96ba553d19d1bc0382fbeb8cc373258b90
FabianFavoryt <91761790+FabianFavoryt@users.noreply.github.com>
2022-08-26 14:26:23 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Csharp1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Once upon a time");
+            Console.WriteLine("I was falling in love");
+            Console.WriteLine("Now I'm only falling apart");
+
+            string text = "Live and sleep!";
+            Console.WriteLine(text);
+
+            string name = "Ada Lovelace";
+            Console.WriteLine(name);
+
+            string input = Console.ReadLine();
+            Console.WriteLine(input);
+            Console.WriteLine(input);
+            Console.WriteLine(input);
+
+            Console.WriteLine("What is your name?");
+            string namn = Console.ReadLine();
+            Console.WriteLine("Välkommen " + namn);
+
+            Console.WriteLine("Hej hur är det?");
+            Console.ReadLine();
+            Console.WriteLine("Vad trevlig, kan du berätta mer?");
+            Console.ReadLine();
+            Console.WriteLine("Nice, ha en bra dag");
+
+
+        }
+    }
+}
